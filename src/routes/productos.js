acdd8d239d4231cb3b71789c7edca2587bdb7e73
Juan david <juandavidx40@gmail.com>
2025-10-03 19:26:41 -0500

    Add Imagenes multiple

--- src/routes/productos.js
+++ src/routes/productos.js
@@ -6,23 +6,23 @@ const pool = require('../db');
 // Listar todos los productos
 router.get('/', async (req, res) => {
     try {
-        const [rows] = await pool.execute(
-            `SELECT p.*, 
-                    u.nombre as vendedor, 
-                    COALESCE(JSON_ARRAYAGG(i.url), JSON_ARRAY()) AS imagenes_producto
-             FROM productos p
-             LEFT JOIN usuarios u ON p.vendedor_id = u.id
-             LEFT JOIN imagenes_productos i ON i.producto_id = p.id
-             GROUP BY p.id
-             ORDER BY p.fecha_creacion DESC`
-        );
-
+        const [rows] = await pool.execute(`
+      SELECT p.*,
+             u.nombre as vendedor,
+             COALESCE(JSON_ARRAYAGG(i.url), JSON_ARRAY()) AS imagenes_producto
+      FROM productos p
+      LEFT JOIN usuarios u ON p.vendedor_id = u.id
+      LEFT JOIN imagenes_producto i ON i.producto_id = p.id
+      GROUP BY p.id
+      ORDER BY p.fecha_creacion DESC
+    `);
         res.json(rows);
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Error servidor' });
     }
 });
+
 
 
 module.exports = router;
